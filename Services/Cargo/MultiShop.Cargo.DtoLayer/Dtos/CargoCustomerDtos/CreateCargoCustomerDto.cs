191325d9542b6c6e51c304c62b506a3dac8672db
BURAK BAYTEMİR <burakbaytemir@posta.mu.edu.tr>
2024-12-22 16:11:04 +0300

    Kargo servisinin tamamlanması

---
+++ Services/Cargo/MultiShop.Cargo.DtoLayer/Dtos/CargoCustomerDtos/CreateCargoCustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Cargo.DtoLayer.Dtos.CargoCustomerDtos
+{
+    public class CreateCargoCustomerDto
+    {
+        public string CargoCustomerName { get; set; }
+        public string CargoCustomerSurname { get; set; }
+        public string CargoCustomerEmail { get; set; }
+        public string CargoCustomerPhone { get; set; }
+        public string CargoCustomerDistrict { get; set; }
+        public string CargoCustomerCity { get; set; }
+        public string CargoCustomerAddress { get; set; }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCustomersController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCustomerDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCustomersController : ControllerBase
+    {
+        private readonly ICargoCustomerService _cargoCustomerService;
+
+        public CargoCustomersController(ICargoCustomerService cargoCustomerService)
+        {
+            _cargoCustomerService = cargoCustomerService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCustomerList()
+        {
+            var values = _cargoCustomerService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCustomer(CreateCargoCustomerDto createCargoCustomerDto)
+        {
+            CargoCustomer cargoCustomer = new CargoCustomer()
+            {
+                CargoCustomerAddress = createCargoCustomerDto.CargoCustomerAddress,
+                CargoCustomerCity = createCargoCustomerDto.CargoCustomerCity,
+                CargoCustomerDistrict = createCargoCustomerDto.CargoCustomerDistrict,
+                CargoCustomerEmail = createCargoCustomerDto.CargoCustomerEmail,
+                CargoCustomerName = createCargoCustomerDto.CargoCustomerName,
+                CargoCustomerPhone = createCargoCustomerDto.CargoCustomerPhone,
+                CargoCustomerSurname = createCargoCustomerDto.CargoCustomerSurname
+            };
+            _cargoCustomerService.TInsert(cargoCustomer);
+            return Ok("Kargo müşterisi oluşturldu");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoCustomer(int id)
+        {
+            _cargoCustomerService.TDelete(id);
+            return Ok("Kargo müşterisi silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCargoCustomerById(int id)
+        {
+            var values = _cargoCustomerService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCustomer(UpdateCargoCustomerDto updateCargoCustomerDto)
+        {
+            CargoCustomer cargoCustomer = new CargoCustomer()
+            {
+                CargoCustomerId = updateCargoCustomerDto.CargoCustomerId,
+                CargoCustomerAddress = updateCargoCustomerDto.CargoCustomerAddress,
+                CargoCustomerCity = updateCargoCustomerDto.CargoCustomerCity,
+                CargoCustomerDistrict = updateCargoCustomerDto.CargoCustomerDistrict,
+                CargoCustomerEmail = updateCargoCustomerDto.CargoCustomerEmail,
+                CargoCustomerName = updateCargoCustomerDto.CargoCustomerName,
+                CargoCustomerPhone = updateCargoCustomerDto.CargoCustomerPhone,
+                CargoCustomerSurname = updateCargoCustomerDto.CargoCustomerSurname
+            };
+            _cargoCustomerService.TUpdate(cargoCustomer);
+            return Ok("Kargo müşterisi güncellendi");
+        }
+    }
+}
