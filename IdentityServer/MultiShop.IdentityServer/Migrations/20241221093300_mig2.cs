6048422e24628b2358c6a0e4fd8c43ac7d54300d
BURAK BAYTEMİR <burakbaytemir@posta.mu.edu.tr>
2024-12-22 09:45:35 +0300

    Identityserver ın kurulması. Port numaralarında revize. Cargo Katmanına başlnaması

---
+++ IdentityServer/MultiShop.IdentityServer/Migrations/20241221093300_mig2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MultiShop.IdentityServer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SurnameName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SurnameName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
