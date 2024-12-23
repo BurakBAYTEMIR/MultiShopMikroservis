06bd32eee893f32d39cefe18f09ded8ba789506e
BURAK BAYTEMİR <burakbaytemir@posta.mu.edu.tr>
2024-12-23 08:58:35 +0300

    Sepet işlemlerinin tamamlanması

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -80,7 +80,7 @@ namespace MultiShop.IdentityServer
             {
                 ClientId="MultiShopAdminId",
                 ClientName="Multi Shop Admin User",
-                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
                 ClientSecrets={new Secret("multishopsecret".Sha256()) },
                 AllowedScopes={ "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "OrderFullPermisson","CargoFullPermission","BasketFullPermission","OcelotFullPermission","CommentFullPermission","PaymentFullPermission","ImageFullPermission","CargoFullPermission",
                 IdentityServerConstants.LocalApi.ScopeName,
---
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
+
+namespace MultiShop.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketsController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasketDetail()
+        {
+            var vales = await _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(vales);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Sepet kaydedildi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet silindi");
+        }
+    }
+}
