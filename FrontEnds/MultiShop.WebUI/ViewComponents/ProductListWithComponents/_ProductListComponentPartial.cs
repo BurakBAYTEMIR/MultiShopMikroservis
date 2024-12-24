e7e1b1463f913da700586f8de05f96030faefee3
BURAK BAYTEMİR <burakbaytemir@posta.mu.edu.tr>
2024-12-24 23:51:24 +0300

    Template Dosyasının yüklenmesi. Default sayfalarının partial lar ile ayrılması ve ana hatlarıyla oluşturulması.

---
+++ FrontEnds/MultiShop.WebUI/ViewComponents/ProductListWithComponents/_ProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListWithComponents
+{
+    public class _ProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
